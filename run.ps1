777dc3ac3b2deb741b7656954b17f6f062f0d8c8
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-03-26 23:01:50 +0100

    feature: add powershell script to run script

---
+++ run.ps1
@@ -0,0 +1,20 @@
+$scriptDirectory = Split-Path -Parent $MyInvocation.MyCommand.Definition
+Set-Location -Path $scriptDirectory
+
+$venvPath = ".venv\Scripts\Activate"
+if (Test-Path $venvPath) {
+    Write-Host "Activating virtual environment..."
+    & $venvPath
+} else {
+    Write-Host "Virtual environment not found."
+    exit 1
+}
+
+$pythonScript = "main.py"
+if (Test-Path $pythonScript) {
+    Write-Host "Running Python script..."
+    python $pythonScript
+} else {
+    Write-Host "Python script 'main.py' not found."
+    exit 1
+}
